f93091ea49fbece8fbf067f93d7ee702b162cf34
Prajwal Mhetre <139099731+pajuu9890@users.noreply.github.com>
2025-03-26 12:45:47 +0530

    Initial commit - Django E-commerce setup

---
+++ store/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import home
+
+urlpatterns = [
+    path('', home, name='home'),
+]
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('store.urls')),  # API Routes
+]
